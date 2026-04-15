f47e00ff48e2a65cc1ad4390c3b00d5eb2643b46
1mahmoudelshora1 <1mahmoudelshora1@gmail.com>
2026-04-15 16:57:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -115,3 +115,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (typeof AOS !== 'undefined') AOS.init({ duration: 1200, once: true });
 });
+document.querySelectorAll('.spritz-neon-card').forEach(card => {
+    card.addEventListener('mousemove', e => {
+        const rect = card.getBoundingClientRect();
+        card.style.setProperty('--x', `${e.clientX - rect.left}px`);
+        card.style.setProperty('--y', `${e.clientY - rect.top}px`);
+    });
+});
